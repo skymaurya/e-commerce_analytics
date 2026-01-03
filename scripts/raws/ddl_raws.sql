0d63f293a597bc495f854f38a6e979e60e4794e1
Akash Maurya <akashcric17@gmail.com>
2026-01-03 12:05:10 +0530

    Add RAW layer DDL and data load stored procedure

---
+++ scripts/raws/ddl_raws.sql
@@ -0,0 +1,179 @@
+/*
+===============================================================================
+DDL Script: Create raws Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'raws' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'raws' Tables
+===============================================================================
+*/
+
+
+
+
+
+------------ customer info tabble   DONE
+
+IF OBJECT_ID('raws.customers_info', 'U') IS NOT NULL
+    DROP TABLE raws.customers_info;
+GO
+CREATE TABLE raws.customers_info(
+    cust_id                 NVARCHAR(100),
+    cust_unique_id          NVARCHAR(80),
+    cust_zip_code_prefix    INT,
+    cust_city               NVARCHAR(50),
+    cust_state              NVARCHAR(50)
+
+
+);
+GO
+
+
+----------- customers location table
+
+
+IF OBJECT_ID('raws.cust_location', 'U') IS NOT NULL
+    DROP TABLE raws.cust_location;
+GO
+
+CREATE TABLE raws.cust_location(
+    loc_zip_code    NVARCHAR(20),
+    loc_latitude    NVARCHAR(50),
+    loc_longitude   NVARCHAR(50),
+    loc_city        NVARCHAR(50),
+    loc_state       NVARCHAR(20)
+);
+GO
+
+-------------  ORDERS_items  TABLE
+
+
+IF OBJECT_ID('raws.orders_items', 'U') IS NOT NULL
+    DROP TABLE raws.orders_items;
+GO
+
+CREATE TABLE raws.orders_items(
+    order_id                NVARCHAR(100),
+    order_item_id           INT,
+    product_id              NVARCHAR(100),
+    seller_id               NVARCHAR(100),
+    shipping_limit_date     DATETIME2,
+    price                   DECIMAL,
+    freight_value           FLOAT
+
+);
+GO
+
+
+----------- orders payment  Table
+
+IF OBJECT_ID('raws.order_payment', 'U') IS NOT NULL
+    DROP TABLE raws.order_payment;
+GO
+
+CREATE TABLE  raws.order_payment(
+    order_id                NVARCHAR(100),
+    payment_sequence        INT,
+    payment_type            NVARCHAR(20),
+    payment_installments    INT,
+    payment_value           DECIMAL
+
+);
+GO
+
+--------- ------orders reviews  table
+
+
+IF OBJECT_ID('raws.orders_reviews', 'U') IS NOT NULL
+    DROP TABLE raws.orders_reviews;
+GO
+
+CREATE TABLE raws.orders_reviews(
+
+    review_id         NVARCHAR(100),
+    order_id          NVARCHAR(100),
+    review_score      INT,
+    review_title      NVARCHAR(255),
+    review_message    NVARCHAR(MAX),
+    review_creation   DATETIME2,
+    review_answer     DATETIME2
+
+
+);
+GO
+
+
+--------------  ORDERS  TABLE
+
+IF OBJECT_ID('raws.orders', 'U') IS NOT NULL
+    DROP TABLE raws.orders;
+GO
+
+CREATE TABLE raws.orders(
+    order_id                        NVARCHAR(100),
+    cust_id                         NVARCHAR(100),
+    order_status                    NVARCHAR(15),
+    ord_purchase_date               DATETIME2,
+    ord_approved_date               DATETIME2,
+    ord_delivered_carrier_date      DATETIME2,
+    ord_delivered_customers_date    DATETIME2,
+    ord_estimated_delivery_date     DATETIME2
+
+);
+GO
+
+------------products info  table
+
+IF OBJECT_ID('raws.products_info', 'U') IS NOT NULL
+DROP TABLE raws.products_info;
+GO
+CREATE TABLE raws.products_info(
+    prd_id                  NVARCHAR(60),
+    prd_catg_name           NVARCHAR(100),
+    prd_name_len            INT,
+    prd_description_len     INT,
+    prd_photo_qty           INT,
+    prd_weight_g            FLOAT,
+    prd_length_cm           FLOAT,
+    prd_height_cm           FLOAT,
+    prd_width_cm            FLOAT
+
+);
+
+GO
+
+
+ -------------SELLERS INFO TABLE
+
+IF OBJECT_ID('raws.sellers_info', 'U') IS NOT NULL
+    DROP TABLE raws.sellers_info;
+GO
+CREATE TABLE raws.sellers_info(
+
+    seller_id               NVARCHAR(60),
+    seller_zip_code_prefix  INT,
+    seller_city             NVARCHAR(50),
+    seller_state            NVARCHAR(20)
+);
+
+GO
+
+
+
+----------product catg_name_ translation
+
+
+IF OBJECT_ID('raws.prd_catg_trans', 'U') IS NOT NULL
+    DROP TABLE raws.prd_catg_trans;
+GO
+
+CREATE TABLE raws.prd_catg_trans(
+    product_catg_name       NVARCHAR(50),
+    product_Eng_name        NVARCHAR(50)
+
+);
+
+GO
+
+
---
+++ scripts/raws/proc_load_raws.sql
@@ -0,0 +1,297 @@
+---MAKING THE STORE PROCEDURE FOR THE 'raws' LAYER
+
+/*
+===============================================================================
+Stored Procedure: Load raws Layer (Source -> raws)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'raws' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC raws.load_raws;
+===============================================================================
+*/
+
+CREATE OR ALTER PROCEDURE raws.load_raws AS 
+
+BEGIN
+     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
+		PRINT '================================================';
+		PRINT 'Loading raws Layer';
+		PRINT '================================================';
+
+		PRINT '------------------------------------------------';
+		PRINT 'Loading customer_info Table';
+		PRINT '------------------------------------------------';
+
+
+        SET @start_time=GETDATE();
+
+        TRUNCATE TABLE raws.customers_info;
+
+        BULK INSERT raws.customers_info
+        FROM 'D:\SQL Project\E-Commerce\datasets\olist_customers_dataset.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+            );
+
+        SET @end_time=GETDATE();
+
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+
+
+        PRINT '------------------------------------------------';
+		PRINT 'Loading cust_location Table';
+		PRINT '------------------------------------------------';
+
+        SET @start_time=GETDATE();
+
+        TRUNCATE TABLE raws.cust_location;
+
+        BULK INSERT raws.cust_location
+        FROM 'D:\SQL Project\E-Commerce\datasets\olist_geolocation_dataset.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+                );
+         SET @end_time=GETDATE();
+
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+
+
+       /* 
+        PRINT '------------------------------------------------';
+		PRINT 'Loading orders_reviews Table';
+		PRINT '------------------------------------------------';
+
+        *----------orders reviews
+
+        SET @start_time=GETDATE();
+        ---using import  method as file was to much messsy to use bulk method
+
+        TRUNCATE TABLE raws.order_reviews;
+
+        BULK INSERT raws.order_reviews
+        FROM 'D:\SQL Project\E-Commerce\datasets\olist_order_reviews_dataset.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+            );
+            
+        SET @end_time=GETDATE();
+
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+        */
+
+        PRINT '------------------------------------------------';
+		PRINT 'Loading orders_reviews Table';
+		PRINT '------------------------------------------------';
+
+
+        SET @start_time=GETDATE();
+
+        TRUNCATE TABLE raws.order_payment;
+
+        BULK INSERT raws.order_payment
+        FROM 'D:\SQL Project\E-Commerce\datasets\olist_order_payments_dataset.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+            );
+         SET @end_time=GETDATE();
+
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+
+
+        PRINT '------------------------------------------------';
+        PRINT 'Loading orders  Table';
+        PRINT '------------------------------------------------';
+
+        SET @start_time=GETDATE();
+
+        TRUNCATE TABLE raws.orders;
+
+        BULK INSERT raws.orders
+        FROM 'D:\SQL Project\E-Commerce\datasets\olist_orders_dataset.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+                );
+        SET @end_time=GETDATE();
+
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+
+
+        PRINT '------------------------------------------------';
+        PRINT 'Loading orders_items Table';
+        PRINT '------------------------------------------------';
+
+        SET @start_time=GETDATE();
+
+        TRUNCATE TABLE raws.orders_items;
+
+        BULK INSERT raws.orders_items
+        FROM 'D:\SQL Project\E-Commerce\datasets\olist_order_items_dataset.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+            );
+
+        SET @end_time=GETDATE();
+
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+
+
+        PRINT '------------------------------------------------';
+        PRINT 'Loading prd_catg_trans Table';
+        PRINT '------------------------------------------------';
+    
+        SET @start_time=GETDATE();
+        TRUNCATE TABLE raws.prd_catg_trans;
+
+        BULK INSERT raws.prd_catg_trans
+        FROM 'D:\SQL Project\E-Commerce\datasets\product_category_name_translation.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+            );
+
+         SET @end_time=GETDATE();
+
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+
+
+        PRINT '------------------------------------------------';
+        PRINT 'Loading product_info Table';
+        PRINT '------------------------------------------------';
+       
+
+        SET @start_time=GETDATE();
+
+        TRUNCATE TABLE raws.products_info;
+
+        BULK INSERT raws.products_info
+        FROM 'D:\SQL Project\E-Commerce\datasets\olist_products_dataset.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+            );
+        SET @end_time=GETDATE();
+
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+
+
+
+        PRINT '------------------------------------------------';
+        PRINT 'Loading sellers_info Table';
+        PRINT '------------------------------------------------';
+   
+
+        SET @start_time=GETDATE();
+
+        TRUNCATE TABLE raws.sellers_info;
+
+        BULK INSERT raws.sellers_info
+        FROM 'D:\SQL Project\E-Commerce\datasets\olist_sellers_dataset.csv'
+        WITH (
+            FORMAT = 'CSV',
+            FIELDTERMINATOR = ',',
+            FIELDQUOTE = '"',
+            FIRSTROW = 2,
+            ROWTERMINATOR = '0x0A',   -- <— important change
+            DATAFILETYPE = 'char',    -- <— required for Express
+            CODEPAGE = '65001',
+            MAXERRORS = 100
+            );
+
+        SET @end_time=GETDATE();
+        PRINT'LOAD TIME -->> :' + CAST( DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT '-------------->'
+
+        
+		SET @batch_end_time = GETDATE();
+		PRINT '=========================================='
+		PRINT 'Loading raws Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
+
+            END TRY
+    BEGIN CATCH
+        PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING raws LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+
+    END CATCH
+
+END
+
+EXEC raws.load_raws;
